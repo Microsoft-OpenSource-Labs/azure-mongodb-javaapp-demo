23f3253f2f13ee9bb53a1dece23083ae9c100573
radim <radim@localhost>
2016-11-23 03:13:18 +0100

    Initial VERSION 1.0 - works with local instance of MongoDB

---
+++ src/main/java/com/microsoft/rci/Employee.java
@@ -0,0 +1,46 @@
+package com.microsoft.rci;
+
+public class Employee {
+
+	private String employeeId;
+	private String type;
+	private String firstName;
+	private String lastName;
+
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setEmployeeId(String string) {
+		employeeId = string;
+		
+	}
+
+	public void setType(String string) {
+		type = string;
+		
+	}
+
+	public void setFirstName(String string) {
+		firstName = string;
+		
+	}
+
+	public void setLastName(String string) {
+		lastName = string;
+		
+	}
+
+}
